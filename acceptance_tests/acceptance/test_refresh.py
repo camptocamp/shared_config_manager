27285be756a3bd20f9943bcdfd5e652f8106f5ce
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2018-08-27 10:05:21 +0200

    Fix coverage

--- acceptance_tests/acceptance/test_refresh.py
+++ acceptance_tests/acceptance/test_refresh.py
@@ -4,3 +4,11 @@ def test_ok(app_connection):
         'status': 200,
         'nb_completed': 2
     }
+
+
+def test_bad_key(app_connection):
+    app_connection.get_json('1/refresh/test_git/bad', expected_status=403)
+
+
+def test_bad_id(app_connection):
+    app_connection.get_json('1/refresh/unknown/changeme', expected_status=404)
