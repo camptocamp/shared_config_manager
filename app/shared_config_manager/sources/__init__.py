42b59375daffb5522e5cb7380340c05176d41ab2
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2018-08-29 15:21:21 +0200

    Added a script to know if the slave is ready

--- app/shared_config_manager/sources/__init__.py
+++ app/shared_config_manager/sources/__init__.py
@@ -2,6 +2,7 @@ from c2cwsgiutils import broadcast
 import logging
 import os
 from pyramid.httpexceptions import HTTPNotFound, HTTPBadRequest
+import tempfile
 from threading import Thread
 import yaml
 
@@ -26,36 +27,45 @@ def init():
     global master_source
     content = yaml.load(os.environ['MASTER_CONFIG'])
     master_source = _create_source(MASTER_ID, content, is_master=True)
+    LOG.info("Initial loading of the master config")
+    master_source.refresh()
     Thread(target=reload_master_config, name="master_config_loader", daemon=True).start()
 
 
 def reload_master_config():
     global sources, master_source
-    LOG.info("Reloading the master config")
-    master_source.refresh()
-    with open(os.path.join(master_source.get_path(), 'shared_config_manager.yaml')) as config_file:
-        config = yaml.load(config_file)
-        if MASTER_ID in config['sources']:
-            raise HTTPBadRequest(f'A source cannot have the "{MASTER_ID}" id')
-        to_deletes = set(sources.keys()) - set(config['sources'].keys())
-        for to_delete in to_deletes:
-            _delete_source(to_delete)
-        for id_, source_config in config['sources'].items():
-            prev_source = sources.get(id_)
-            if prev_source is None:
-                LOG.info("New source detected: %s", id_)
-            elif prev_source.get_config() == source_config:
-                LOG.debug("Source %s didn't change, not reloading it", id_)
-                continue
-            else:
-                LOG.info("Change detected in source %s, reloading it", id_)
-                _delete_source(id_)  # to be sure the old stuff is cleaned
+    _update_flag("LOADING")
+    try:
+        with open(os.path.join(master_source.get_path(), 'shared_config_manager.yaml')) as config_file:
+            config = yaml.load(config_file)
+            if MASTER_ID in config['sources']:
+                raise HTTPBadRequest(f'A source cannot have the "{MASTER_ID}" id')
+            to_deletes = set(sources.keys()) - set(config['sources'].keys())
+            for to_delete in to_deletes:
+                _delete_source(to_delete)
+            for id_, source_config in config['sources'].items():
+                prev_source = sources.get(id_)
+                if prev_source is None:
+                    LOG.info("New source detected: %s", id_)
+                elif prev_source.get_config() == source_config:
+                    LOG.debug("Source %s didn't change, not reloading it", id_)
+                    continue
+                else:
+                    LOG.info("Change detected in source %s, reloading it", id_)
+                    _delete_source(id_)  # to be sure the old stuff is cleaned
 
-            try:
-                sources[id_] = _create_source(id_, source_config)
-                sources[id_].refresh()
-            except Exception:
-                LOG.error("Cannot load the %s config", id_, exc_info=True)
+                try:
+                    sources[id_] = _create_source(id_, source_config)
+                    sources[id_].refresh()
+                except Exception:
+                    LOG.error("Cannot load the %s config", id_, exc_info=True)
+    finally:
+        _update_flag("READY")
+
+
+def _update_flag(value):
+    with open(os.path.join(tempfile.gettempdir(), 'status'), 'w') as flag:
+        flag.write(value)
 
 
 def _delete_source(id_):
@@ -67,7 +77,9 @@ def _delete_source(id_):
 @broadcast.decorator(expect_answers=True, timeout=120)
 def refresh(id_, key):
     config = _get_source(id_, key)
+    LOG.info("Reloading the %s config", id_)
     if config.is_master():
+        config.refresh()
         reload_master_config()
     else:
         config.refresh()
