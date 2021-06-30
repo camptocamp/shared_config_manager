750412a040a7ddb5c4b411c22acf356b6dc10281
Stéphane Brunner <stephane.brunner@camptocamp.com>
2021-06-30 15:12:37 +0200

    Upgrade for Python 3.6 (with pyupgrade)

--- acceptance_tests/acceptance/__init__.py
+++ acceptance_tests/acceptance/__init__.py
@@ -10,7 +10,7 @@ def wait_sync(app_connection, name, hash_):
             if hash_ is None:
                 if name in slave["sources"]:
                     raise RuntimeError(f"{name} still found in sources")
-                print("Name '{}' still found in sources".format(name))
+                print(f"Name '{name}' still found in sources")
             else:
                 if name not in slave["sources"]:
                     raise RuntimeError(f"In salve {slave_name}, {name} not in {slave['sources'].keys()}")
@@ -23,7 +23,7 @@ def wait_sync(app_connection, name, hash_):
                         f"Wrong hash for slave {slave_name}, source {name}: "
                         f"{slave['sources'][name]['hash']} != {hash_}"
                     )
-                print("Name '{}' and hash found in sources".format(name))
+                print(f"Name '{name}' and hash found in sources")
         return True
 
     utils.retry_timeout(what, timeout=10, interval=1)
